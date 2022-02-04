2ca5ccee1d37bae6893c77e3a8fdf7f5c908940e
Mehmet <96449723+Mehmet-rg@users.noreply.github.com>
2022-02-04 14:02:26 +0300

    Update videostream.py

--- Yukki/Utilities/videostream.py
+++ Yukki/Utilities/videostream.py
@@ -52,7 +52,7 @@ async def start_stream_video(message, file, title, mystic):
         final_output = await message.reply_photo(
             photo="Utils/Telegram.JPEG",
             caption=(
-                f"🎬<b>__Video:__ </b> [Given Video Via Telegram]({link})\n\n👤<b>__Requested by:__ </b>{message.from_user.mention} \n🚧<b>__Queued at:__</b> <b>#{position}!</b>"
+                f"🎥<b> 𝖵𝗂𝖽𝖾𝗈 : </b> [Telegram Yoluyla Verilen Video]({link})\n\n📝<b> 𝖳𝖺𝗅𝖾𝗉: </b>{message.from_user.mention} \n🚧<b> 𝖲𝗂𝗋𝖺𝗒𝖺 𝖺𝗅𝗂𝗇𝖽𝗂:</b> <b>#{position}!</b>"
             ),
             reply_markup=audio_markup2,
         )
@@ -61,7 +61,7 @@ async def start_stream_video(message, file, title, mystic):
     else:
         if not await join_video_stream(message.chat.id, file, 720):
             return await mystic.edit(
-                "Error Joining Voice Chat. Make sure Voice Chat is Enabled."
+                "Sesli Sohbete Katılma Hatası. Sesli Sohbetin Etkin olduğundan emin olun."
             )
         get_queue[message.chat.id] = []
         got_queue = get_queue.get(message.chat.id)
@@ -75,7 +75,7 @@ async def start_stream_video(message, file, title, mystic):
         await add_active_video_chat(message.chat.id)
         buttons = secondary_markup2("Smex1", message.from_user.id)
         await mystic.delete()
-        cap = f"🎥<b>__Playing:__ </b>[Given Video Via Telegram]({link})\n👤**__Requested by:__** {message.from_user.mention}"
+        cap = f"🎥<b> 𝖮𝗒𝗇𝗎𝗒𝗈𝗋: </b>[Telegram Yoluyla Verilen Video]({link})\n📝** 𝖳𝖺𝗅𝖾𝗉:** {message.from_user.mention}"
         final_output = await message.reply_photo(
             photo="Utils/Telegram.JPEG",
             reply_markup=InlineKeyboardMarkup(buttons),
@@ -118,7 +118,7 @@ async def start_live_stream(
     await add_active_chat(CallbackQuery.message.chat.id)
     await add_active_video_chat(CallbackQuery.message.chat.id)
     buttons = secondary_markup2(videoid, CallbackQuery.from_user.id)
-    cap = f"**Live Streaming**\n\n🎥<b>__Playing:__ </b>[{title[:25]}](https://www.youtube.com/watch?v={videoid}) \n💡<b>__Info:__</b> [Get Additional Information](https://t.me/{BOT_USERNAME}?start=info_{videoid})\n👤**__Requested by:__** {CallbackQuery.from_user.mention}"
+    cap = f"**𝖢𝖺𝗇𝗅𝗂 𝖸𝖺𝗒𝗂𝗇**\n\n🎥<b> 𝖮𝗒𝗇𝗎𝗒𝗈𝗋: </b>[{title[:25]}](https://www.youtube.com/watch?v={videoid}) \n💡<b> 𝖡𝗂𝗅𝗀𝗂:</b> [Ek Bilgi Alın](https://t.me/{BOT_USERNAME}?start=info_{videoid})\n📝** 𝖳𝖺𝗅𝖾𝗉:** {CallbackQuery.from_user.mention}"
     final_output = await CallbackQuery.message.reply_photo(
         photo=thumb,
         reply_markup=InlineKeyboardMarkup(buttons),
@@ -172,7 +172,7 @@ async def start_video_stream(
         final_output = await CallbackQuery.message.reply_photo(
             photo=thumb,
             caption=(
-                f"🎬<b>Video:__ </b>[{title[:25]}](https://www.youtube.com/watch?v={videoid}) \n⏳<b>__Duration:__</b> {duration_min} \n💡<b>__Info:__</b> [Get Additional Information](https://t.me/{BOT_USERNAME}?start=info_{videoid})\n👤<b>__Requested by:__ </b>{CallbackQuery.from_user.mention} \n🚧<b>__ Video Queued at:__</b> <b>#{position}!</b>"
+                f"🎥<b> 𝖵𝗂𝖽𝖾𝗈: </b>[{title[:25]}](https://www.youtube.com/watch?v={videoid}) \n⌚<b> 𝖲𝗎𝗋𝖾:</b> {duration_min} \n💡<b> 𝖡𝗂𝗅𝗀𝗂:</b> [Ek Bilgi Alın](https://t.me/{BOT_USERNAME}?start=info_{videoid})\n📝<b> 𝖳𝖺𝗅𝖾𝗉: </b>{CallbackQuery.from_user.mention} \n🚧<b> 𝖲𝗂𝗋𝖺𝗒𝖺 𝖠𝗅𝗂𝗇𝖽𝗂:</b> <b>#{position}!</b>"
             ),
             reply_markup=InlineKeyboardMarkup(buttons),
         )
@@ -200,7 +200,7 @@ async def start_video_stream(
         buttons = primary_markup(
             videoid, CallbackQuery.from_user.id, duration_min, duration_min
         )
-        cap = f"**Video Streaming**\n\n🎥<b>__Playing:__ </b>[{title[:25]}](https://www.youtube.com/watch?v={videoid}) \n💡<b>__Info:__</b> [Get Additional Information](https://t.me/{BOT_USERNAME}?start=info_{videoid})\n👤**__Requested by:__** {CallbackQuery.from_user.mention}"
+        cap = f"**𝖵𝗂𝖽𝖾𝗈 𝖠𝗄𝗂𝗌𝗂**\n\n🎥<b> 𝖮𝗒𝗇𝗎𝗒𝗈𝗋: </b>[{title[:25]}](https://www.youtube.com/watch?v={videoid}) \n💡<b> 𝖡𝗂𝗅𝗀𝗂:</b> [Ek Bilgi Alın](https://t.me/{BOT_USERNAME}?start=info_{videoid})\n📝** 𝖳𝖺𝗅𝖾𝗉:** {CallbackQuery.from_user.mention}"
         final_output = await CallbackQuery.message.reply_photo(
             photo=thumb,
             reply_markup=InlineKeyboardMarkup(buttons),

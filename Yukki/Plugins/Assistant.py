7c54f4e395870d9c72aa8c5830a6d2925553fff1
Mehmet <96449723+Mehmet-rg@users.noreply.github.com>
2022-02-04 15:08:21 +0300

    Update Assistant.py

--- Yukki/Plugins/Assistant.py
+++ Yukki/Plugins/Assistant.py
@@ -11,40 +11,25 @@ from Yukki import ASSISTANT_PREFIX, SUDOERS, app, random_assistant
 from Yukki.Database import get_assistant, save_assistant
 from Yukki.Utilities.assistant import get_assistant_details
 
-__MODULE__ = "Assistant"
+__MODULE__ = "𝖠𝗌𝗂𝗌𝗍𝖺𝗇"
 __HELP__ = f"""
 
 
+» /oynat 𝖪𝗈𝗆𝗎𝗍𝗎 𝗂𝗅𝖾 𝖺𝗌𝗂𝗌𝗍𝖺𝗇𝗂 𝗀𝗋𝗎𝖻𝗎𝗇𝗎𝗓𝖺 𝖽𝖺𝗏𝖾𝗍 𝖾𝖽𝖾𝖻𝗂𝗅𝗂𝗋𝗌𝗂𝗇𝗂𝗓 .
+
 /checkassistant
-- Check the alloted assistant of your chat
+- Sohbetinizin tahsis edilen yardımcısını kontrol edin
 
 
-**Note:**
-- Only for Sudo Users
+**Not:**
+- Sadece Sudo Kullanıcıları için
 
-{ASSISTANT_PREFIX[0]}block [ Reply to a User Message] 
-- Blocks the User from Assistant Account.
 
-{ASSISTANT_PREFIX[0]}unblock [ Reply to a User Message] 
-- Unblocks the User from Assistant Account.
+/changeassistant [ASS NUMARA]
+- Önceki tahsis edilmiş asistanı yenisiyle değiştirin.
 
-{ASSISTANT_PREFIX[0]}approve [ Reply to a User Message] 
-- Approves the User for DM.
-
-{ASSISTANT_PREFIX[0]}disapprove [ Reply to a User Message] 
-- Disapproves the User for DM.
-
-{ASSISTANT_PREFIX[0]}pfp [ Reply to a Photo] 
-- Changes Assistant account PFP.
-
-{ASSISTANT_PREFIX[0]}bio [Bio text] 
-- Changes Bio of Assistant Account.
-
-/changeassistant [ASS NUMBER]
-- Change the previoius alloted assistant to new one.
-
-/setassistant [ASS NUMBER or Random]
-- Set a assistant account for chat. 
+/setassistant [ASS NUMARA]
+- Sohbet için bir asistan hesabı ayarlayın. 
 """
 
 
@@ -53,7 +38,7 @@ ass_num_list = ["1", "2", "3", "4", "5"]
 
 @app.on_message(filters.command("changeassistant") & filters.user(SUDOERS))
 async def assis_change(_, message: Message):
-    usage = f"**Usage:**\n/changeassistant [ASS_NO]\n\nSelect from them\n{' | '.join(ass_num_list)}"
+    usage = f"**kullanım:**\n/changeassistant [ASS_NO]\n\nAralarından seçim yapın\n{' | '.join(ass_num_list)}"
     if len(message.command) != 2:
         return await message.reply_text(usage)
     num = message.text.split(None, 1)[1].strip()
@@ -94,7 +79,7 @@ async def assis_change(_, message: Message):
     _assistant = await get_assistant(message.chat.id, "assistant")
     if not _assistant:
         await message.reply_text(
-            f"**__Yukki Music Bot Assistant Alloted__**\n\nAssistant No. **{ran_ass}**"
+            f"**__Star Music Bot Assistant Alloted__**\n\nAssistant No. **{ran_ass}**"
         )
         assis = {
             "saveassistant": ran_ass,

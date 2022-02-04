109926e3ceafa3a6a15dc2df4b1187bb7426f484
Mehmet <96449723+Mehmet-rg@users.noreply.github.com>
2022-02-04 14:29:56 +0300

    Update permission.py

--- Yukki/Decorators/permission.py
+++ Yukki/Decorators/permission.py
@@ -10,27 +10,27 @@ def PermissionCheck(mystic):
         a = await app.get_chat_member(message.chat.id, BOT_ID)
         if a.status != "administrator":
             return await message.reply_text(
-                "I need to be admin with some permissions:\n"
-                + "\n- **can_manage_voice_chats:** To manage voice chats"
-                + "\n- **can_delete_messages:** To delete Bot's Searched Waste"
-                + "\n- **can_invite_users**: For inviting assistant to chat."
+                "𝖡𝖺𝗓𝗂 𝗂𝗓𝗂𝗇𝗅𝖾𝗋𝗅𝖾 𝗒𝗈𝗇𝖾𝗍𝗂𝖼𝗂 𝗈𝗅𝗆𝖺𝗆 𝗀𝖾𝗋𝖾𝗄𝗂𝗒𝗈𝗋:\n"
+                + "\n- **can_manage_voice_chats:** 𝖲𝖾𝗌𝗅𝗂 𝖲𝗈𝗁𝖻𝖾𝗍𝗅𝖾𝗋𝗂 𝖸𝗈𝗇𝖾𝗍𝗆𝖾𝗄"
+                + "\n- **can_delete_messages:** 𝖠𝗋𝖺𝗇𝖺𝗇 𝖠𝗍𝗂𝗄𝗅𝖺𝗋𝗂 𝖳𝖾𝗆𝗂𝗓𝗅𝖾𝗆𝖾𝗄"
+                + "\n- **can_invite_users**: 𝖠𝗌𝗂𝗌𝗍𝖺𝗇𝗂 𝖣𝖺𝗏𝖾𝗍 𝖾𝗍𝗆𝖾𝗄"
             )
         if not a.can_manage_voice_chats:
             await message.reply_text(
-                "I don't have the required permission to perform this action."
-                + "\n**Permission:** __MANAGE VOICE CHATS__"
+                "𝖡𝗎 𝖤𝗒𝗅𝖾𝗆𝗂 𝖦𝖾𝗋𝖼𝖾𝗄𝗅𝖾𝗌𝗍𝗂𝗋𝗆𝖾𝗄 𝗂𝖼𝗂𝗇 𝖦𝖾𝗋𝖾𝗄𝗅𝗂 𝗂𝗓𝗇𝖾 𝖲𝖺𝗁𝗂𝗉 𝖣𝖾𝗀𝗂𝗅𝗂𝗆."
+                + "\n**𝗂𝗓𝗂𝗇:** 𝖲𝖾𝗌𝗅𝗂 𝖲𝗈𝗁𝖻𝖾𝗍𝗅𝖾𝗋𝗂 𝖸𝗈𝗇𝖾𝗍𝗆𝖾"
             )
             return
         if not a.can_delete_messages:
             await message.reply_text(
-                "I don't have the required permission to perform this action."
-                + "\n**Permission:** __DELETE MESSAGES__"
+                "𝖡𝗎 𝖤𝗒𝗅𝖾𝗆𝗂 𝖦𝖾𝗋𝖼𝖾𝗄𝗅𝖾𝗌𝗍𝗂𝗋𝗆𝖾𝗄 𝗂𝖼𝗂𝗇 𝖦𝖾𝗋𝖾𝗄𝗅𝗂 𝗂𝗓𝗇𝖾 𝖲𝖺𝗁𝗂𝗉 𝖣𝖾𝗀𝗂𝗅𝗂𝗆."
+                + "\n**𝗂𝗓𝗂𝗇:** 𝖬𝖾𝗌𝖺𝗃𝗅𝖺𝗋𝗂 𝖲𝗂𝗅𝗆𝖾 "
             )
             return
         if not a.can_invite_users:
             await message.reply_text(
-                "I don't have the required permission to perform this action."
-                + "\n**Permission:** __INVITE USERS VIA LINK__"
+                "𝖡𝗎 𝖤𝗒𝗅𝖾𝗆𝗂 𝖦𝖾𝗋𝖼𝖾𝗄𝗅𝖾𝗌𝗍𝗂𝗋𝗆𝖾𝗄 𝗂𝖼𝗂𝗇 𝖦𝖾𝗋𝖾𝗄𝗅𝗂 𝗂𝗓𝗇𝖾 𝖲𝖺𝗁𝗂𝗉 𝖣𝖾𝗀𝗂𝗅𝗂𝗆."
+                + "\n**𝗂𝗓𝗂𝗇:** 𝖡𝖺𝗀𝗅𝖺𝗇𝗍𝗂 𝖽𝖺𝗏𝖾𝗍 𝖾𝗍𝗆𝖾"
             )
             return
         return await mystic(_, message)

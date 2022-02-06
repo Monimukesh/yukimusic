12f7c39174bcec4a2a409a98ab1411513c59550d
Mehmet <96449723+Mehmet-rg@users.noreply.github.com>
2022-02-06 23:25:45 +0300

    Update videocalls.py

--- Yukki/Inline/videocalls.py
+++ Yukki/Inline/videocalls.py
@@ -6,17 +6,17 @@ def choose_markup(videoid, duration, user_id):
     buttons = [
         [
             InlineKeyboardButton(
-                text="🎙️ 𝖬𝗎𝗓𝗂𝗄 𝖢𝖺𝗅",
+                text="🎙️ ᴍᴜᴢɪᴋ ᴄᴀʟ",
                 callback_data=f"MusicStream {videoid}|{duration}|{user_id}",
             ),
             InlineKeyboardButton(
-                text="🎥 𝖵𝗂𝖽𝖾𝗈 𝖨𝗓𝗅𝖾",
+                text="🎥 ᴠɪᴅᴇᴏ ɪᴢʟᴇ",
                 callback_data=f"Choose {videoid}|{duration}|{user_id}",
             ),
         ],
         [
             InlineKeyboardButton(
-                text="🗑 𝖠𝗋𝖺𝗆𝖺𝗒𝗂 𝖪𝖺𝗉𝖺𝗍",
+                text="🗑 ᴀʀᴀᴍᴀʏɪ ᴋᴀᴘᴀᴛ",
                 callback_data=f"forceclose {videoid}|{user_id}",
             )
         ],
@@ -28,11 +28,11 @@ def livestream_markup(quality, videoid, duration, user_id):
     buttons = [
         [
             InlineKeyboardButton(
-                text="🎥 𝖢𝖺𝗇𝗅𝗂 𝖡𝖺𝗌𝗅𝖺𝗍",
+                text="🎥 ᴄᴀɴʟɪ ʙᴀsʟᴀᴛ",
                 callback_data=f"LiveStream {quality}|{videoid}|{duration}|{user_id}",
             ),
             InlineKeyboardButton(
-                text="🗯️ 𝖪𝖺𝗉𝖺𝗍",
+                text="🗯️ ᴋᴀᴘᴀᴛ",
                 callback_data=f"forceclose {videoid}|{user_id}",
             ),
         ],
@@ -44,25 +44,25 @@ def stream_quality_markup(videoid, duration, user_id):
     buttons = [
         [
             InlineKeyboardButton(
-                text="📽 360P",
+                text="📽 360ᴘ",
                 callback_data=f"VideoStream 360|{videoid}|{duration}|{user_id}",
             ),
             InlineKeyboardButton(
-                text="📽 720P",
+                text="📽 720ᴘ",
                 callback_data=f"VideoStream 720|{videoid}|{duration}|{user_id}",
             ),
             InlineKeyboardButton(
-                text="📽 480P",
+                text="📽 480ᴘ",
                 callback_data=f"VideoStream 480|{videoid}|{duration}|{user_id}",
             ),
         ],
         [
             InlineKeyboardButton(
-                text="🛠️ 𝖦𝖾𝗋𝗂 𝖦𝗂𝗍",
+                text="🛠️ ɢᴇʀɪ ɢɪᴛ",
                 callback_data=f"gback_list_chose_stream {videoid}|{duration}|{user_id}",
             ),
             InlineKeyboardButton(
-                text="🗯️ 𝖠𝗋𝖺𝗆𝖺𝗒𝗂 𝖪𝖺𝗉𝖺𝗍",
+                text="🗯️ ᴀʀᴀᴍᴀʏɪ ᴋᴀᴘᴀᴛ",
                 callback_data=f"forceclose {videoid}|{user_id}",
             ),
         ],
